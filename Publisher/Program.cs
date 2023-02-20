880138a12ba010b4d8a85fbb59c567abf6dd8f1f
yunusefendi52 <>
2023-02-20 10:47:20 +0700

    fdaf

---
+++ Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using Google.Api.Gax;
+using Google.Cloud.PubSub.V1;
+
+Console.WriteLine("Starting publisher...");
+
+var projectId = "project-app-d1471";
+var topicId = "logging-api";
+
+var publisherService = await new PublisherServiceApiClientBuilder
+{
+    EmulatorDetection = EmulatorDetection.EmulatorOrProduction
+}.BuildAsync();
+
+var topicName = new TopicName(projectId, topicId);
+
+if (publisherService.GetTopic(topicName) == null)
+{
+    await publisherService.CreateTopicAsync(topicName);
+}
+
+var publisher = await PublisherClient.CreateAsync(topicName);
+
+Console.WriteLine("Type message");
+
+while (true)
+{
+    var text = Console.ReadLine();
+
+    await publisher.PublishAsync(text);
+
+    Console.WriteLine($"SENDING: {text}");
+
+    if (text?.Trim().ToLowerInvariant() == "q")
+    {
+        break;
+    }
+}
+
+await publisher.ShutdownAsync(TimeSpan.FromSeconds(15));
---
+++ Subscriber/Program.cs
@@ -0,0 +1,57 @@
+﻿using Google.Api.Gax;
+using Google.Cloud.PubSub.V1;
+using static Google.Cloud.PubSub.V1.PublisherClient;
+
+Console.WriteLine("Starting subscriber...");
+
+var projectId = "project-app-d1471";
+// var topicId = "logging-api";
+var subscriptionId = "logging-api-sub";
+
+// var subscriberService = await new SubscriberServiceApiClientBuilder
+// {
+//     EmulatorDetection = EmulatorDetection.EmulatorOrProduction
+// }.BuildAsync();
+
+var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+// var topicName = new TopicName(projectId, topicId);
+
+// if (subscriberService.GetSubscription(subscriptionName) == null)
+// {
+//     await subscriberService.CreateSubscriptionAsync(subscriptionName,
+//         topicName, pushConfig: null, ackDeadlineSeconds: 60);
+// }
+
+var pullMode = false;
+if (pullMode)
+{
+    var subscriber = await SubscriberServiceApiClient.CreateAsync();
+    var response = await subscriber.PullAsync(subscriptionName, 2);
+    foreach (ReceivedMessage received in response.ReceivedMessages)
+    {
+        PubsubMessage msg = received.Message;
+        Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+        Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+    }
+
+    await subscriber.AcknowledgeAsync(subscriptionName, response.ReceivedMessages.Select(m => m.AckId));
+}
+else
+{
+    var subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+
+    Console.WriteLine("Waiting some messages...");
+
+    await subscriber.StartAsync(async (msg, cancellationToken) =>
+    {
+        Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+        Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+
+        if (msg.Data.ToStringUtf8() == "q")
+        {
+            await subscriber.StopAsync(TimeSpan.FromSeconds(15));
+        }
+
+        return SubscriberClient.Reply.Ack;
+    });
+}
